630606579e6bbecf68abf4de03c9503b98d1cbd1
ShenHongFei <shen.hongfei@outlook.com>
2023-06-19 16:55:42 +0800

    add use_modal;

---
+++ modal.ts
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+export function use_modal () {
+    const [visible, set_visible] = useState(false)
+    
+    return {
+        visible,
+        
+        open () {
+            set_visible(true)
+        },
+        
+        close () {
+            set_visible(false)
+        }
+    }
+}
