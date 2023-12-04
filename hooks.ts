285b7f1461d0d7466788f87d70d0c753d9911a83
ShenHongFei <shen.hongfei@outlook.com>
2023-12-04 11:36:09 +0800

    use_modal moved to hooks.ts; add use_rerender;

--- hooks.ts
+++ hooks.ts
@@ -1,6 +1,15 @@
 import { useState } from 'react'
 
 
+export function use_rerender () {
+    const set_state = useState({ })[1]
+    
+    return function rerender () {
+        set_state({ })
+    }
+}
+
+
 export function use_modal () {
     const [visible, set_visible] = useState(false)
     
